e3fc9ca8cc5e5092474d7a4da13080ac79c310b0
desarrollo <desarrollo@academicos.cesde.edu.co>
2025-05-13 17:20:09 -0500

    Se configura opciones

--- listado.py
+++ listado.py
@@ -32,16 +32,26 @@ print("3. Modificar una tarea")
 print("4. Eliminar una tarea")
 print("5. SALIR")
 
-
+listaTareas=[]
 while opcion != 5:
+    tarea={}
     opcion=int(input("Digita una opcion por favor: "))
     if opcion == 1:
-        pass
+        print("Creando tarea....")
+        tarea['id']=input("Digita el id de la tarea:")
+        tarea['descripcion']=input("Digita una descripcion:")
+        tarea['diaSemana']=input("¿Que dia es esta tarea?: ")
+        tarea['hora']=input("¿A que hora es esta tarea?:")
+        tarea['fecha']=input("Digita la fecha del evento (AAA-MM-DD)")
+        listaTareas.append(tarea)
     elif opcion == 2:
-        pass
+        print("Consultando las taras....")
+        print(listaTareas)
     elif opcion == 3:
+        print("Modificando una  taras....")
         pass 
     elif opcion == 4:
+        print("Eliminando una  taras....")
         pass
     elif opcion == 5:
         print("saliendo")
